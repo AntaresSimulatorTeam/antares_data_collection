5603d8c66c77be33bb7ca30d309d2d754ae20ba3
belthlemar <martin.belthle@rte-france.com>
2026-03-03 18:10:12 +0100

    add real test case

--- tests/antares/thermal/test_install_power.py
+++ tests/antares/thermal/test_install_power.py
@@ -11,17 +11,30 @@
 # This file is part of the Antares project.
 from pathlib import Path
 
+import pandas as pd
+
 from antares.data_collection.referential_data.main_params import parse_main_params
 from antares.data_collection.thermal.constants import (
     THERMAL_INSTALL_POWER_FOLDER,
 )
 from antares.data_collection.thermal.parsing.installed_power import ThermalInstallerPowerParser
+from tests.conftest import RESOURCE_PATH
 
 
-def test_truc(tmp_path: Path) -> None:
-    resource_path = Path("/home/belthlemar/Projects/Antares/antares_data_collection/tests/antares/resources")
-    main_params = parse_main_params(resource_path / "MAIN_PARAMS_2025.xlsx")
-    parser = ThermalInstallerPowerParser(resource_path, tmp_path, ["Available on market"], main_params, [2030])
+def test_nominal_case(tmp_path: Path) -> None:
+    # Use the real MainParams file
+    main_params = parse_main_params(RESOURCE_PATH / "MAIN_PARAMS_2025.xlsx")
+
+    # Build a thermal installed power file
+    parser = ThermalInstallerPowerParser(RESOURCE_PATH, tmp_path, ["Available on market"], main_params, [2030])
     parser.build_thermal_installed_power()
-    assert (tmp_path / THERMAL_INSTALL_POWER_FOLDER / "thermal_installed_power.xlsx").exists()
-    print("ok")
+
+    # Asserts the file is created
+    generated_file_path = tmp_path / THERMAL_INSTALL_POWER_FOLDER / "thermal_installed_power.xlsx"
+    assert generated_file_path.exists()
+    generated_df = pd.read_excel(generated_file_path)
+
+    # Compare its content with the expected one
+    expected_file_path = RESOURCE_PATH / "expected_output_files" / "thermal_installed_power.xlsx"
+    expected_df = pd.read_excel(expected_file_path)
+    pd.testing.assert_frame_equal(generated_df, expected_df, check_dtype=False)
