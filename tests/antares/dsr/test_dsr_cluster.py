269356bc0b37730d8922c061a341eacde59af140
berthetclement <clement.berthet013@gmail.com>
2026-04-07 14:22:04 +0200

    tests: add DSR cluster test with assertions and duration measurement

--- tests/antares/dsr/test_dsr_cluster.py
+++ tests/antares/dsr/test_dsr_cluster.py
@@ -9,9 +9,12 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-
+import time
 from pathlib import Path
 
+import pandas as pd
+
+from antares.data_collection.dsr.constants import DSR_FOLDER
 from antares.data_collection.dsr.parsing import DsrParser
 from antares.data_collection.referential_data.main_params import parse_main_params
 from tests.conftest import RESOURCE_PATH
@@ -25,4 +28,32 @@ def test_nominal_case(tmp_path: Path) -> None:
     parser = DsrParser(
         RESOURCE_PATH, tmp_path, ["Available on market", "Inelastic supply / fixed profile"], main_params, [2030, 2035]
     )
+
+
+    start = time.time()
     parser.build_dsr_cluster()
+    end = time.time()
+    print("Duration DSR Cluster", end - start)
+
+    # Asserts the file is created
+    generated_file_path = tmp_path / DSR_FOLDER / "cluster_DSR.xlsx"
+    assert generated_file_path.exists()
+
+    # read Excel workbook, one sheet by year
+    file_wb = pd.ExcelFile(generated_file_path)
+    sheet_names = file_wb.sheet_names
+
+    generated_df = pd.read_excel(generated_file_path, sheet_name=sheet_names)
+    assert list(generated_df.keys()) == ['2030', '2035']
+
+    # Compare its content with the expected one for any sheet
+    # 2030
+    expected_file_path = RESOURCE_PATH / "expected_output_files" / "dsr" / "dsr_cluster_2030.xlsx"
+    expected_df_2030 = pd.read_excel(expected_file_path)
+    sheet_name = list(generated_df.keys())[0]
+    pd.testing.assert_frame_equal(generated_df[sheet_name], expected_df_2030, check_dtype=False)
+    # 2035
+    expected_file_path = RESOURCE_PATH / "expected_output_files" / "dsr" / "dsr_cluster_2035.xlsx"
+    expected_df_2035 = pd.read_excel(expected_file_path)
+    sheet_name = list(generated_df.keys())[1]
+    pd.testing.assert_frame_equal(generated_df[sheet_name], expected_df_2035, check_dtype=False)
