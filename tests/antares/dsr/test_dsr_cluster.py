ae35944be70322077f1398a1200b1390c786c8a9
berthetclement <clement.berthet013@gmail.com>
2026-04-24 16:55:20 +0200

    test: add validation for DSR capacity modulation output files
    - Extended tests to validate the generation and content of DSR capacity modulation files.
    - Introduced assertions for correctness of generated file paths, sheet names, and content comparison against expected outputs.

--- tests/antares/dsr/test_dsr_cluster.py
+++ tests/antares/dsr/test_dsr_cluster.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 import pandas as pd
 
-from antares.data_collection.dsr.constants import DSR_CLUSTER_FOLDER
+from antares.data_collection.dsr.constants import DSR_CAPACITY_MODULATION_FOLDER, DSR_CLUSTER_FOLDER
 from antares.data_collection.dsr.parsing import DsrParser
 from antares.data_collection.referential_data.main_params import parse_main_params
 from tests.conftest import RESOURCE_PATH
@@ -52,8 +52,37 @@ def test_nominal_case(tmp_path: Path) -> None:
     generated_df = pd.read_excel(generated_file_path, sheet_name=sheet_names)
     assert list(generated_df.keys()) == ["2030", "2035"]
 
+    ##
+    # DSR CLUSTER
+    ##
     # Compare its content with the expected one for any sheet
     expected_wb_file_path = RESOURCE_PATH / "expected_output_files" / "dsr" / "cluster_DSR.xlsx"
+    expected_wb = pd.ExcelFile(expected_wb_file_path)
+    # 2030
+    sheet_name = list(generated_df.keys())[0]
+    expected_df_2030 = pd.read_excel(expected_wb, sheet_name=sheet_name)
+    pd.testing.assert_frame_equal(generated_df[sheet_name], expected_df_2030, check_dtype=False)
+    # 2035
+    sheet_name = list(generated_df.keys())[1]
+    expected_df_2035 = pd.read_excel(expected_wb, sheet_name=sheet_name)
+    pd.testing.assert_frame_equal(generated_df[sheet_name], expected_df_2035, check_dtype=False)
+
+    ##
+    # DSR CAPACITY MODULATION
+    ##
+
+    generated_file_path = tmp_path / DSR_CAPACITY_MODULATION_FOLDER / "capacity_modulation_DSR.xlsx"
+    assert generated_file_path.exists()
+
+    # read Excel workbook, one sheet by year
+    file_wb = pd.ExcelFile(generated_file_path)
+    sheet_names = file_wb.sheet_names
+
+    generated_df = pd.read_excel(generated_file_path, sheet_name=sheet_names)
+    assert list(generated_df.keys()) == ["2029-2030", "2034-2035"]
+
+    # Compare its content with the expected one for any sheet
+    expected_wb_file_path = RESOURCE_PATH / "expected_output_files" / "dsr" / "capacity_modulation_DSR.xlsx"
     expected_wb = pd.ExcelFile(expected_wb_file_path)
     # 2030
     sheet_name = list(generated_df.keys())[0]
