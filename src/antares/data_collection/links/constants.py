4c83224dabee27f3437f998470f2399133b75784
berthetclement <clement.berthet013@gmail.com>
2026-05-05 18:10:57 +0200

    feat: introduce links parsing and filtering with tests and input files
    - Added `LinksParser` class for parsing and filtering links data based on scenarios and year ranges.
    - Implemented new utility function `filter_based_on_year_range`.
    - Introduced new constants in `constants.py` to support links data parsing.
    - Added unit tests covering nominal cases, edge cases, and export validations.
    - Included sample input files for testing links processing.

--- src/antares/data_collection/links/constants.py
+++ src/antares/data_collection/links/constants.py
@@ -9,4 +9,62 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
+from enum import StrEnum
 
+LINKS_NTC_INDEX_NAME = "NTCs Index.csv"
+LINKS_NTC_TS_NAME = "NTCs.csv"
+LINKS_TRANSFER_LINKS_NAME = "Transfer Links.csv"
+
+NTC_FILTER_STR_VALUE = "NTC"
+
+
+# data "Transfer Links.csv"
+class InputTransferLinksColumns(StrEnum):
+    ZONE = "ZONE"
+    MARKET_ZONE_SOURCE = "MARKET_ZONE_SOURCE"
+    MARKET_ZONE_DESTINATION = "MARKET_ZONE_DESTINATION"
+    TRANSFER_TYPE = "TRANSFER_TYPE"
+    STUDY_SCENARIO = "STUDY_SCENARIO"
+    YEAR_VALID_START = "YEAR_VALID_START"
+    YEAR_VALID_END = "YEAR_VALID_END"
+    TRANSFER_TECHNOLOGY = "TRANSFER_TECHNOLOGY"
+    NTC_LIMIT_CAPACITY_STATIC = "NTC_LIMIT_CAPACITY_STATIC"
+    NTC_CURVE_ID = "NTC_CURVE_ID"
+    NO_POLES = "NO_POLES"
+    FOR = "FOR"
+
+
+# "NTCs Index.csv"
+class InputNTCsIndexColumns(StrEnum):
+    CURVE_UID = "CURVE_UID"
+    ZONE = "ZONE"
+    ID = "ID"
+
+
+# "NTCs.csv"
+class InputNTCsColumns(StrEnum):
+    MONTH = "MONTH"
+    DAY = "DAY"
+    HOUR = "HOUR"
+
+
+# columns for export links file
+class ExportLinksColumnsNames(StrEnum):
+    NAME = "Name"
+
+    WINTER_HP_DIRECT_MW = "Winter_HP_Direct_MW"
+    WINTER_HP_INDIRECT_MW = "Winter_HP_Indirect_MW"
+    WINTER_HC_DIRECT_MW = "Winter_HC_Direct_MW"
+    WINTER_HC_INDIRECT_MW = "Winter_HC_Indirect_MW"
+
+    SUMMER_HP_DIRECT_MW = "Summer_HP_Direct_MW"
+    SUMMER_HP_INDIRECT_MW = "Summer_HP_Indirect_MW"
+    SUMMER_HC_DIRECT_MW = "Summer_HC_Direct_MW"
+    SUMMER_HC_INDIRECT_MW = "Summer_HC_Indirect_MW"
+
+    FLOWBASED_PERIMETER = "Flowbased_perimeter"
+
+    HVDC = "HVDC"
+
+    SPECIFIC_TS = "Specific_TS"
+    FORCED_OUTAGE_HVAC = "Forced_Outage_HVAC"
--- src/antares/data_collection/links/parsing.py
+++ src/antares/data_collection/links/parsing.py
@@ -9,4 +9,55 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
+from pathlib import Path
 
+import pandas as pd
+
+from antares.data_collection.links.constants import (
+    LINKS_TRANSFER_LINKS_NAME,
+    NTC_FILTER_STR_VALUE,
+    InputTransferLinksColumns,
+)
+from antares.data_collection.referential_data.main_params import MainParams
+from antares.data_collection.utils import (
+    filter_based_on_study_scenarios,
+    filter_based_on_year_range,
+    filter_non_declared_areas,
+    parse_input_file,
+)
+
+
+class LinksParser:
+    def __init__(self, input_folder: Path, output_folder: Path, main_params: MainParams, years: list[int]):
+        self.input_folder = input_folder
+        self.output_folder = output_folder
+        self.main_params = main_params
+        self.years = years
+
+    def _parse_transfer_links(self) -> pd.DataFrame:
+        return parse_input_file(self.input_folder / LINKS_TRANSFER_LINKS_NAME, list(InputTransferLinksColumns))
+
+    def _filter_based_on_ntc(self, ntc_name_column: str, df: pd.DataFrame) -> pd.DataFrame:
+        if ntc_name_column not in df.columns:
+            raise ValueError(f"Column {ntc_name_column} not found in the dataframe 'Transfer Links'")
+        return df[df[ntc_name_column] == NTC_FILTER_STR_VALUE]
+
+    def build_links(self) -> None:
+        df = self._parse_transfer_links()
+
+        # transfer links file pre filter
+        df = filter_non_declared_areas(self.main_params, df, InputTransferLinksColumns.MARKET_ZONE_SOURCE)
+        df = filter_non_declared_areas(self.main_params, df, InputTransferLinksColumns.MARKET_ZONE_DESTINATION)
+
+        df = filter_based_on_study_scenarios(
+            df, self.main_params, self.years, InputTransferLinksColumns.STUDY_SCENARIO.value
+        )
+        df = filter_based_on_year_range(
+            df,
+            self.years,
+            InputTransferLinksColumns.YEAR_VALID_START.value,
+            InputTransferLinksColumns.YEAR_VALID_END.value,
+        )
+        df = self._filter_based_on_ntc(InputTransferLinksColumns.TRANSFER_TYPE, df)
+
+        # mapping with index/time series
